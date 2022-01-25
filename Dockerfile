8b5e82bd39aef12dbd79b727d255d1747ea8e359
c2io <c2io@outlook.com>
2022-01-26 00:26:51 +0800

    build: docker:bullseye build->slim run

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,21 @@
-FROM python:3.10
-
-RUN pip install poetry -i https://pypi.tuna.tsinghua.edu.cn/simple \
-    # && poetry config virtualenvs.create false
-    && poetry config virtualenvs.in-project true 
-
-WORKDIR /app
+FROM python:bullseye as builder
 
 COPY poetry.lock pyproject.toml ./
-RUN poetry install
+RUN python -m venv /venv \
+    && pip install poetry -i https://pypi.tuna.tsinghua.edu.cn/simple \
+    && . /venv/bin/activate \
+    && poetry install --no-dev
 
+
+FROM python:slim as runner
+
+COPY --from=builder /venv /venv
+
+WORKDIR /app
 COPY app.py ./
 
 ENV STREAMLIT_SERVER_ADDRESS=0.0.0.0
 ENV STREAMLIT_SERVER_PORT=8080
 EXPOSE ${STREAMLIT_SERVER_PORT}
 
-CMD ["poetry", "run", "streamlit", "run", "app.py"]
+CMD ["/venv/bin/streamlit", "run", "app.py"]
--- app.py
+++ app.py
@@ -1,49 +1,59 @@
-from typing import Tuple
-from dataclasses import dataclass
+from typing import Dict, List
 
 import streamlit as st
 import pendulum as dt
 
 
-@dataclass
 class ConditionScore:
-    conditions: Tuple
-    scores: Tuple
+    """condition:score"""
 
+    def __init__(self, data: Dict[str, float]) -> None:
+        self._data = data
 
-def ratio_label(cs: ConditionScore) -> str:
-    labels = []
-    for condition, score in zip(cs.conditions, cs.scores):
-        labels.append(f"{condition} {score}分")
+    @property
+    def label(self) -> str:
+        labels = []
+        for condition, score in self._data.items():
+            labels.append(f"{condition} {score}分")
+        return ", ".join(labels)
 
-    return ", ".join(labels)
+    @property
+    def conditions(self) -> List[str]:
+        return self._data.keys()
+
+    def score(self, condition: str) -> float:
+        return self._data[condition]
 
 
 st.title("上海新房积分计算器")
-
+st.write(
+    "上海买房实施积分摇号制度，购房人在认购后，"
+    "将综合家庭、户籍、拥有的住房状况、五年内在沪购房记录以及在沪缴纳社保五大因素拥有一个积分，"
+    "然后根据摇号人数比房源多30%的原则，按积分高低排序选取进入公证摇号选房的人员名单。"
+)
 score = 0
 
 st.subheader("家庭情况")
-cs = ConditionScore(["家庭", "单身"], [10, 0])
-family = st.radio(ratio_label(cs), cs.conditions, 1)
-score += cs.scores[0] if family == cs.conditions[0] else cs.scores[1]
+cs = ConditionScore(dict(家庭=10, 单身=0))
+family = st.radio(cs.label, cs.conditions, 1)
+score += cs.score(family)
 
 st.subheader("户籍情况")
-cs = ConditionScore(["上海户口", "非上海户口"], [10, 0])
-house_hold = st.radio(ratio_label(cs), cs.conditions)
-score += cs.scores[0] if house_hold == cs.conditions[0] else cs.scores[1]
+cs = ConditionScore(dict(上海户口=10, 非上海户口=0))
+house_hold = st.radio(cs.label, cs.conditions, 0)
+score += cs.score(house_hold)
 
 st.subheader("房产情况")
-cs = ConditionScore(["上海无房", "上海有房"], [20, 5])
-house = st.radio(ratio_label(cs), cs.conditions)
-score += cs.scores[0] if house == cs.conditions[0] else cs.scores[1]
+cs = ConditionScore(dict(上海无房=20, 上海有房=5))
+house = st.radio(cs.label, cs.conditions, 0)
+score += cs.score(house)
 
 st.subheader("五年内购房情况")
 cs = ConditionScore(
-    ["无房-5年内无购房记录", "有房-5年内无购房记录", "无房-5年内有购房记录", "有房-5年内有购房记录"], [20, 5, 0, 0]
+    {"无房-5年内无购房记录": 20, "有房-5年内无购房记录": 5, "无房-5年内有购房记录": 0, "有房-5年内有购房记录": 0}
 )
-house_record = st.radio(ratio_label(cs), cs.conditions)
-score += cs.scores[cs.conditions.index(house_record)]
+house_record = st.radio(cs.label, cs.conditions, 0)
+score += cs.score(house_record)
 
 st.subheader("基础分")
 st.write(f"总计: {score}")
@@ -58,7 +68,7 @@ social_insurance_months = (
 st.write(f"总计: {social_insurance_months}")
 
 st.subheader("社保系数")
-social_insurance_coefficient = st.slider("社保缴纳系数(0.1-0.24)", 0.1, 0.24, 0.13)
+social_insurance_coefficient = st.slider("社保缴纳系数(0.1-0.24)", 0.10, 0.24, 0.13)
 
 st.subheader("总分")
 st.write(f"总计: {score+social_insurance_months*social_insurance_coefficient}")
